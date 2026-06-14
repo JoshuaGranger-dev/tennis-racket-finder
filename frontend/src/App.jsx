f86c5adadaef312bc588184988061b8d45ae4566
skycastle359 <159149960+skycastle359@users.noreply.github.com>
2026-06-14 17:54:28 -0400

    Added search bar for brand and model

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,8 @@ import RacketList from "./components/RacketList"
 function App() {
   const [rackets, setRackets] = useState([])
   const [selectedBrand, setSelectedBrand] = useState("All")
-  const [selectedStringPattern, setSelectedStringPattern] =useState("All")
+  const [selectedStringPattern, setSelectedStringPattern] = useState("All")
+  const [searchTerm, setSearchTerm] = useState("")
 
   useEffect(() => {
     console.log("useEffect is running")
@@ -30,21 +31,36 @@ function App() {
       filteredRackets = filteredRackets.filter((racket) => racket.stringPattern === selectedStringPattern)
     }
 
+    if (searchTerm !== "") {
+      filteredRackets = filteredRackets.filter((racket) => 
+        racket.brand.toLowerCase().includes(searchTerm.toLowerCase())
+        || racket.model.toLowerCase().includes(searchTerm.toLowerCase())
+      )
+    }
+
   return (
     <div>
       <h1>Tennis Racket Finder</h1>
+      <input type="text"
+        value={searchTerm} 
+        onChange={(e) => setSearchTerm(e.target.value)}
+        placeholder ="Search by brand or model"
+        />
+
       <select value={selectedBrand} onChange={(e) => setSelectedBrand(e.target.value)}>
         <option value="All">All</option>
         <option value="Wilson">Wilson</option>
         <option value="Babolat">Babolat</option>
         <option value="Yonex">Yonex</option>
       </select>
+
       <select value={selectedStringPattern} onChange={(e) => setSelectedStringPattern(e.target.value)}>
         <option value="All">All</option>
         <option value="16x19">16x19</option>
         <option value="18x20">18x20</option>
         <option value="16x20">16x20</option>
       </select>
+
       <RacketList rackets={filteredRackets} />
     </div>
   )
