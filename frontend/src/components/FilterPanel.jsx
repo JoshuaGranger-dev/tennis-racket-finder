258af4f855e6d36bf789587c857247fc4db40bde
skycastle359 <159149960+skycastle359@users.noreply.github.com>
2026-06-15 16:49:33 -0400

    Filter Clear Button Added

--- frontend/src/components/FilterPanel.jsx
+++ frontend/src/components/FilterPanel.jsx
@@ -6,6 +6,13 @@ function FilterPanel({
     selectedStringPattern,
     setSelectedStringPattern,
  }) {
+
+    function handleClearFilters() {
+        setSearchTerm("")
+        setSelectedBrand("All")
+        setSelectedStringPattern("All")
+    }
+
     return (
         <div>
             <input type="text"
@@ -14,19 +21,28 @@ function FilterPanel({
             placeholder="Search by brand or model"
             />
 
-            <select value={selectedBrand} onChange={(e) => setSelectedBrand(e.target.value)}>
+            <select value={selectedBrand} 
+            onChange={(e) => setSelectedBrand(e.target.value)}
+            >
                 <option value="All">All</option>
                 <option value="Wilson">Wilson</option>
                 <option value="Babolat">Babolat</option>
                 <option value="Yonex">Yonex</option>
             </select>
 
-            <select value={selectedStringPattern} onChange={(e) => setSelectedStringPattern(e.target.value)}>
+            <select value={selectedStringPattern} 
+            onChange={(e) => setSelectedStringPattern(e.target.value)}
+            >
                 <option value="All">All</option>
                 <option value="16x19">16x19</option>
                 <option value="18x20">18x20</option>
                 <option value="16x20">16x20</option>
             </select>
+
+            <button onClick={handleClearFilters}>
+                Clear Filters
+            </button>
+
       </div>
     )
 }
