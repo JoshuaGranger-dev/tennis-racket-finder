a02a2d812bc5e60f1f245bf958a8bd9b6736468e
skycastle359 <159149960+skycastle359@users.noreply.github.com>
2026-07-21 16:03:07 -0400

    Use environment variables for database config

--- backend/db.js
+++ backend/db.js
@@ -1,11 +1,13 @@
+require("dotenv").config()
+
 const { Pool } = require("pg")
 
 const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "tennis-racket-finder",
-  password: "@Lsujag359",
-  port: 5432,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: Number(process.env.DB_PORT),
 })
 
 module.exports = pool
